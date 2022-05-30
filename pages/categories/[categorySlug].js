10cef57c18ce353007606f238108da781c05ed51
Corey DeLoach <cdeloach16@gmail.com>
2022-05-30 11:03:13 -0500

    added buttons to categories pages

--- pages/categories/[categorySlug].js
+++ pages/categories/[categorySlug].js
@@ -3,6 +3,7 @@ import styles from "../../styles/Home.module.css";
 import Head from "next/head";
 import Image from "next/image";
 import { buildImage } from "../../lib/cloudinary";
+import Link from "next/link";
 
 export async function getStaticProps({ params }) {
   const client = new ApolloClient({
@@ -102,15 +103,32 @@ export default function Category({ category, products }) {
             .toURL();
           return (
             <div key={product.id} style={{ margin: "1rem" }}>
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-              <Image
-                className={styles.productImage}
-                src={imageURL}
-                alt={`${product.name} by Ashley Gooler | Iron & Clay Studio`}
-                height="665"
-                width="500"
-              />
+              <Link href={`/products/${product.slug}`}>
+                <div>
+                  <h2>{product.name}</h2>
+                  <p>${product.price}</p>
+                  <Image
+                    className={styles.productImage}
+                    src={imageURL}
+                    alt={`${product.name} by Ashley Gooler | Iron & Clay Studio`}
+                    height="665"
+                    width="500"
+                  />
+                </div>
+              </Link>
+
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <button
+                  className={`snipcart-add-item ${styles.largeCardButton}`}
+                  data-item-id={product.id}
+                  data-item-price={product.price}
+                  data-item-url={`/products/${product.slug}`}
+                  data-item-image={product.image.url}
+                  data-item-name={product.name}
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
           );
         })}
